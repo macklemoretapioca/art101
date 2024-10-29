bdba42ad008013c26f0bcaed9235e4af70ea440a
macklemoretapioca <cjmaynar@ucsc.edu>
2024-10-28 22:39:04 -0700

    Update lab.js

--- Lab7/js/lab.js
+++ Lab7/js/lab.js
@@ -3,18 +3,22 @@
 // Date: 10/28/24
 
 // Sortusername - a function that sorts the letter of the input
-function sortusername() {
+function sortUserName() {
   var userName = window.prompt("Hi. PLease tell me your name so I can fix it.");
   console.log("userName=", userName);
+
   // split string to array
   var nameArray = userName.split('');
   console.log("nameArray =", nameArray);
+
   // sort the array
   var nameArraySort = nameArray.sort();
   console.log("nameArraySort =", nameArraySort);
+
   // Join array back to a string
   var nameSorted = nameArraySort.join('');
   console.log("nameSorted =", nameSorted);
+
   // Did the long version only to work on the function elements
   return nameSorted;
 }
