109769b017ce1a9ccef7cde31c746d35a497fd2e
macklemoretapioca <cjmaynar@ucsc.edu>
2024-10-30 16:37:43 -0700

    Update lab.js

--- Lab8/js/lab.js
+++ Lab8/js/lab.js
@@ -3,13 +3,27 @@
 // Date: 10/28/24
 
 //function to calculate square root
-function calculateSquare(x){
+function MultiplyDis(x){
   var results = x * x;
   return results;
 }
 
 //print result
-console.log(calculateSquare(5));
+console.log("Square these up", MultiplyDis(5));
+
+array = [3, 19, 44, 98]
+console.log("My array", array);
+
+var result = array.map(MultiplyDis);
+//will return multiplied numbers 
+console.log("Test of square on array", result);
+
+var result = array.map (function(x){
+  return x/4;
+})
+//should return array values /4
+console.log("Array divided by 4", result);
+
 
 
 
